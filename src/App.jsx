64a2b35540bc05c8f0f53c4a30f67b6b0a3d95c9
devilghostg <mister.devil6@gmail.com>
2025-05-16 12:45:48 +0400

    First commit app home

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,38 @@
 import './App.css'
 import Fournisseur from "./Components/Fournisseur.jsx";
-import {BrowserRouter, Route, Router, Routes} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
 
-  const NotFound = () => {
-    return <>
-      <h1>Not Found</h1>
-    </>
-  }
-
   return (
     <>
-      <Routes>
-        <Route path="/fournisseur" component={Fournisseur} />
-        <Route path="*" component={NotFound} />
-      </Routes>
+      <header>
+        <h1>Gestion Pièces Auto</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="/" title="Accueil">
+                <i className="fas fa-home"></i> Accueil
+              </a>
+            </li>
+            <li>
+              <a href="/fournisseur" title="Gestion des fournisseurs">
+                <i className="fas fa-truck"></i> Fournisseurs
+              </a>
+            </li>
+            <li>
+              <a href="/piece" title="Gestion des pièces">
+                <i className="fas fa-cogs"></i> Pièces
+              </a>
+            </li>
+            <li>
+              <a href="/stock" title="Gestion du stock">
+                <i className="fas fa-warehouse"></i> Stock
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </>
   )
 }
